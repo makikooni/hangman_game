e234a05ad9a744e23352dbe1332ded6d34699297
Weronika E Falinska <w.e.falinska@gmail.com>
2023-11-13 16:47:47 +0000

    visibility_of_the_word

--- hangman.py
+++ hangman.py
@@ -22,27 +22,44 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
     user_lives = 5
+    
+    #print(word)
+        
     print(f'Your word has {len(word_letters)} letters.')
     
-    #getting user input
+        
+    
+   
     while user_lives > 0 and word_letters: 
+        if used_letters:
+            print(f'Your used letters:', ' '.join(used_letters))
+        
+        #current_word
+        word_list = [letter if letter in used_letters else '-' for letter in word]    
+        print('Your word: ', ' '.join(word_list))
+            
+            
         user_letter = input("Guess a letter: ").upper()
+        
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
                 print("Good guess!")
                 print(f'Your word: ')
-                print(f'Your used letters: {used_letters}')
             else:
                 user_lives -= 1
-                print(f'Your letter {user_letter} is not in the word.\nYou lost one life. You have {user_lives} lives remaining.\nYour used letters: {used_letters}')
+                print(f'Your letter {user_letter} is not in the word.\nYou lost one life. You have {user_lives} lives remaining.')
                 if user_lives <= 0:
                     print("Game over. You lost all your lives.")
                     play_again()
+        elif user_letter in used_letters:
+            print('You cannot reuse the same letter.') 
         else:
-            print(f'You cannot reuse the same letter.\nYour used letters: {used_letters}') 
-
+            print("Invalid character. Please try again.")
+            
+        
    
         
 hangman()
+
